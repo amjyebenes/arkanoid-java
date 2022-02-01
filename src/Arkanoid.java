d45e12a9e58199ac09ca17f31baa5fa4714b9b82
Ángel Manuel Joaquín Yébenes <amjyebenes@gmail.com>
2022-02-01 18:06:03 +0100

    mensaje de commit

--- src/Arkanoid.java
+++ src/Arkanoid.java
@@ -20,7 +20,7 @@ public class Arkanoid extends JFrame {
     }
 
     public static void main(String[] args) {
-        ventana = new JFrame("Space Invaders");
+        ventana = new JFrame("Arkanoid");
         ventana.addWindowListener(new WindowAdapter() {
 
         });
